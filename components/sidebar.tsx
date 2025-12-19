6d42235c3039077fa3cd8821f1f6cd2534a410b4
Leonard <Oseghaleleonard39@gmail.com>
2025-12-19 19:05:59 +0100

    feat: finished the sidebar somehow

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -14,7 +14,7 @@ const Sidebar = ({
   return (
     <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between border-r border-gray-200 bg-white pt-8! text-white max-md:hidden sm:p-4! xl:p-6! 2xl:w-[355px]">
       <nav className="flex flex-col gap-4">
-        <Link href="/" className='mb-12 cursor-pointer items-center gap-2 flex flex-row'>
+        <Link href="/" className='mb-12 cursor-pointer items-center gap-2 flex'>
           <Image
             src="/icons/logo.svg"
             width={34} height={34}
@@ -44,7 +44,9 @@ const Sidebar = ({
                     className={cn({ 'brightness-[3]! invert-0': isActive })}
                   />
                 </div>
-                {label}
+                <p className={cn('text-16 font-semibold text-black-2 max-xl:hidden', { 'text-white!': isActive })}>
+                  {label}
+                </p>
               </Link>
             )
           })
